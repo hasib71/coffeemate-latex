e637d023f93473d89b54db89ab04df7db1a06b87
Hasib <himuhasib@gmail.com>
2018-10-26 22:11:09 +0600

    Remove fixed CTAN mirror

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 mkdir -p playground/install-tl-unx
 cd playground
-wget http://mirrors.huaweicloud.com/repository/toolkit/CTAN/systems/texlive/tlnet/install-tl-unx.tar.gz
+wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 tar -xvf install-tl-unx.tar.gz -C install-tl-unx --strip-components=1
 cd install-tl-unx
-echo I | ./install-tl --location http://mirrors.huaweicloud.com/repository/toolkit/CTAN/systems/texlive/tlnet
+echo I | ./install-tl
 cd ../..
 rm -rf playground
